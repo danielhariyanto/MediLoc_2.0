3723599e57b619731e5a1b6d58d1788e6826494a
Meshach Adoe <madoe@ucsd.edu>
2020-12-31 10:19:53 +0700

    Setup react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './styles/style.scss'
+import logo from './assets/mediloc-logo.svg'
+
+function App() {
+  	return (
+    	<div className="App">
+			<div className="main">
+				<img src={logo} alt="Mediloc Logo"/>
+
+			</div>
+    	</div>
+  	);
+}
+
+export default App;
